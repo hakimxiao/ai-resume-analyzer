d7d9e42bebce792481563990d03d239c411994f4
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-16 09:24:19 +0700

    - FINISHING FILE UPLOADER -
    * Menyelesaikan fungsionalitas depan untuk upload file dan untuk fungsionalitas belakang akan di lanjutkan nanti.

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -7,12 +7,28 @@ const Upload = () => {
     const [statusText, setStatusText] = useState("");
     const [file, setFile] = useState<File | null>(null);
 
-
     const handleFileSelect = (file: File | null) => {
         setFile(file);
     }
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {}
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const form = e.currentTarget.closest("form");
+        if(!form) return;
+        const formData = new FormData(form);
+
+        const companyName = formData.get("company-name");
+        const jobTitle = formData.get("job-title");
+        const jobDescription = formData.get("job-description");
+
+        console.log({
+            companyName,
+            jobTitle,
+            jobDescription,
+            file
+        })
+    }
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
