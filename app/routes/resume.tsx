f265deb6d9c0eb42183b40671a5e59a8bfc1606a
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-18 10:32:47 +0700

    - MEMBUAT 3 COMPONENT BARU YANG AKAN MENYEMPURNAKAN UI DARI resume.tsx PAGE -

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import {Link, useNavigate, useParams} from "react-router";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 export const meta = () => ([
     { title: "Resumind | Review"},
@@ -12,9 +15,13 @@ const Resume = () => {
     const { id } = useParams();
     const [imageUrl, setImageUrl] = useState("");
     const [resumeUrl, setResumeUrl] = useState("");
-    const [feedback, setFeedback] = useState("");
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
 
+
+    useEffect(() => {
+        if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume${id}`);
+    }, [isLoading]);
 
     useEffect(() => {
         const loadResume = async() => {
@@ -58,6 +65,18 @@ const Resume = () => {
                                 <img src={imageUrl} alt="url" title="resume" className="w-full h-full object-contain rounded-2xl" />
                             </a>
                         </div>
+                    )}
+                </section>
+                <section className="feedback-section">
+                    <h2 className="text-4xl text-black font-bold">Resume Review</h2>
+                    {feedback ? (
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+                            <Summary feedback={feedback} />
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <Details feedback={feedback} />
+                        </div>
+                    ) : (
+                        <img src="/images/resume-scan-2.gif" alt="logo" className="w-full" />
                     )}
                 </section>
             </div>
